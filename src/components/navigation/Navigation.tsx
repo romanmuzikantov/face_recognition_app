24eceddee612e5ad96da90ed45d90beb48f35eaf
Roman Muzikantov <roman.muzikantov@gmail.com>
2023-07-02 21:10:44 +0200

    importing parallax tilt and adding logo

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Tilt from "react-parallax-tilt";
+import Brain from "./brain.png";
 
 const Navigation = () => {
 	return (
-		<nav className="flex justify-end pr3 pl3">
-			<p className="f4 black dim link pa3 pointer outline br2">Sign Out</p>
+		<nav className="flex justify-between items-center pa3">
+			<Tilt className="w3 h3 tilt shadow-1 flex items-center justify-center pa2 outline">
+				<img src={Brain}></img>
+			</Tilt>
+			<p className="f4 black dim link pa3 pointer outline br2 ma0">Sign Out</p>
 		</nav>
 	)
 }
