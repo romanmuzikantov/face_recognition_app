0b5412c6bdebd7c8ff77ead479314321f2aee2c4
Roman Muzikantov <roman.muzikantov@gmail.com>
2023-07-02 20:10:44 +0200

    importing tachyons and styling sign out button

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,11 +1,9 @@
-import 'react';
+import React from "react";
 
 const Navigation = () => {
 	return (
-		<nav>
-			<p>
-				Sign Out
-			</p>
+		<nav className="flex justify-end pr3 pl3">
+			<p className="f4 black dim link pa3 pointer outline br2">Sign Out</p>
 		</nav>
 	)
 }
