ad18054a973a6987c141233886d92a0b23ee2224
Roman Muzikantov <roman.muzikantov@gmail.com>
2023-07-05 16:53:39 +0200

    setting up form actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,44 @@
-import React, { useCallback } from 'react';
+import React, { BaseSyntheticEvent, Component, useCallback } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Ranks';
 import CustomParticles from './components/Particles/Particles';
 
-const App = () => {
+interface AppState {
+  input: string
+}
+
+class App extends Component {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event: BaseSyntheticEvent) => {
+    console.log(event.target.value);
+    console.log((this.state as AppState).input);
+  }
+
+  onFormSubmit = () => {
+    console.log("click");
+  }
+
+  render() {
     return (
       <div className="App flex flex-column">
         <Navigation />
-        <div className='justify-center flex flex-column' style={{gap: '24px', flex: '1'}}>
+        <div className='flex flex-column' style={{gap: '24px', flex: '1'}}>
           <Rank />
-          <ImageLinkForm />
+          <ImageLinkForm onInputChange={this.onInputChange} onFormSubmit={this.onFormSubmit} />
         </div>
         <CustomParticles />
       </div>
     );
+  }
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.tsx
+++ src/components/ImageLinkForm/ImageLinkForm.tsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { BaseSyntheticEvent } from "react";
 
-const ImageLinkForm = () => {
+interface ImageLinkFormProps {
+    onInputChange: (event: BaseSyntheticEvent) => void
+    onFormSubmit: () => void
+}
+
+const ImageLinkForm = ({ onInputChange, onFormSubmit }: ImageLinkFormProps) => {
     return (
-        <div className="br2 bg-white-70 shadow-1 w-50 pa4 center z-9999">
+        <div className="br2 bg-white-70 shadow-1 w-50 pa4 center z-9999 ba b--black">
             <p className="f4 mt0 mb4">
                 {'This Magic Brain will detect faces in images. Give it a try!'}
             </p>
             <div className="flex items-center justify-center" style={{gap: '0.8rem'}}>
-                <input type="text" className="w-70 f4 pa2 center"/>
-                <button className="w-30 grow f4 link pv2 dib black br2 ph4 bg-washed-green pointer outline-0 ba border-color hover-near-white bg-animate hover-bg-near-black">
+                <input type="text" className="w-70 f4 pa2 center" onChange={onInputChange}/>
+                <button className="w-30 grow f4 link pv2 dib black br2 ph4 bg-washed-green pointer outline-0 ba border-color hover-near-white bg-animate hover-bg-near-black"
+                onClick={onFormSubmit}>
                     Detect
                 </button>
             </div>
