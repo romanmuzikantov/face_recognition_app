a035d86a4f1dcad589e15932cd52871594cf53a6
romanmuzikantov <roman.muzikantov@gmail.com>
2023-07-07 15:51:30 +0200

    making the layout more round

--- src/components/ImageLinkForm/ImageLinkForm.tsx
+++ src/components/ImageLinkForm/ImageLinkForm.tsx
@@ -9,19 +9,21 @@ interface ImageLinkFormProps {
 
 const ImageLinkForm = ({ onInputChange, onFormSubmit }: ImageLinkFormProps) => {
     return (
-        <div className="w-50-l w-90-ns pa4 center z-9999 form-card">
-            <div className="flex flex-column" style={{gap: '24px'}}>
-                <Rank />
-                <p className="f4 mt0 mb4">
-                    {'This Magic Brain will detect faces in images. Give it a try!'}
-                </p>
-            </div>
-            <div className="flex items-center justify-center" style={{gap: '0.8rem'}}>
-                <input type="text" className="w-70 f4 pa2 center" onChange={onInputChange}/>
-                <button className="w-30 grow f4 link pv2 dib black br2 ph4 bg-washed-green pointer outline-0 ba border-color hover-near-white bg-animate hover-bg-near-black"
-                onClick={onFormSubmit}>
-                    Detect
-                </button>
+        <div className="flex w-100 justify-center">
+            <div className="w-50-l w-100-ns pa4 z-9999 ml3-ns mr3-ns br4 form-card">
+                <div className="flex flex-column" style={{gap: '24px'}}>
+                    <Rank />
+                    <p className="f4 mt0 mb4">
+                        {'This Magic Brain will detect faces in images. Give it a try!'}
+                    </p>
+                </div>
+                <div className="flex items-center justify-center" style={{gap: '0.8rem'}}>
+                    <input type="text" className="w-70 f4 pa2 center br4 ba b--near-black" onChange={onInputChange}/>
+                    <button className="w-30 grow f4 link pv2 dib black br4 ph4 bg-washed-green pointer outline-0 ba b--near-black hover-near-white bg-animate hover-bg-near-black"
+                    onClick={onFormSubmit}>
+                        Detect
+                    </button>
+                </div>
             </div>
         </div>
     )
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -5,14 +5,14 @@ import './Navigation.css';
 
 const Navigation = () => {
 	return (
-		<nav className="flex justify-between items-center z-9999 navigation pt3-l pb3-l pt2-ns pb2-ns pl4 pr4">
+		<nav className="flex justify-between items-center z-9999 navigation pt3-l pb3-l pt2-ns pb2-ns pl4-l pl3-ns pr4-l pr3-ns mt3 mr4-l mr3-ns ml4-l ml3-ns br4">
 			<div className="flex items-center" style={{gap: '24px'}}>
-				<Tilt className="w3 h3 tilt shadow-2 flex items-center justify-center pa2 outline background" perspective={250} tiltMaxAngleX={35} tiltMaxAngleY={35}>
+				<Tilt className="w3 h3 tilt shadow-2 flex items-center justify-center pa2 outline background br4" perspective={250} tiltMaxAngleX={35} tiltMaxAngleY={35}>
 					<img src={Brain}></img>
 				</Tilt>
 				<p className="f3 ma0">{'SmartBrain'}</p>
 			</div>
-			<p className="f4 black link pa3 pointer outline br2 ma0 bg-animate hover-bg-white-80 shadow-hover">Sign Out</p>
+			<p className="f4 black link pa3 pointer outline br4 ma0 bg-animate hover-bg-white-80 shadow-hover">Sign Out</p>
 		</nav>
 	)
 }
