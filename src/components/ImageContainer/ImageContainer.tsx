25f67b27c6e0bd414d7295ff499cf46e151a19bc
Roman Muzikantov <roman.muzikantov@gmail.com>
2023-07-14 00:15:11 +0200

    drawing rectangles based on bounding boxes

--- src/components/ImageContainer/ImageContainer.tsx
+++ src/components/ImageContainer/ImageContainer.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { BoundingBox } from '../../models/ClarifaiFaceDetectionResponse';
+import './ImageContainer.css';
 
 interface ImageContainerProps {
     imageUrl: string;
@@ -8,9 +9,34 @@ interface ImageContainerProps {
 
 function ImageContainer({ imageUrl, boundingBoxes }: ImageContainerProps): JSX.Element {
     console.log(boundingBoxes);
+    const ref: React.MutableRefObject<HTMLCanvasElement | null> = useRef(null);
+
+    useEffect(() => {
+        const canvas: HTMLCanvasElement | null = ref.current;
+        if (canvas != null) {
+            const context = canvas.getContext('2d');
+            if (context != null) {
+                context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+                context.strokeStyle = '#ff4136';
+
+                boundingBoxes.forEach((val) => {
+                    const xPos = val.left_col * context.canvas.width;
+                    const yPos = val.top_row * context.canvas.height;
+                    const width = val.right_col * context.canvas.width - xPos;
+                    const height = val.bottom_row * context.canvas.height - yPos;
+
+                    context.strokeRect(xPos, yPos, width, height);
+                });
+            }
+        }
+    }, [boundingBoxes]);
+
     return (
-        <div className="z-9999">
-            <img alt="" src={imageUrl} />
+        <div className="flex justify-center">
+            <div className="z-9999 ml3 mr3 canvas-container">
+                <canvas ref={ref} className="canvas" />
+                <img alt="" src={imageUrl} />
+            </div>
         </div>
     );
 }
